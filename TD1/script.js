bcaaba4f3b42c050fd5e1fa1f92bba9e2e847050
langloi2415 <arthur.langlois@etu.unicaen.fr>
2025-09-15 16:56:30 +0200

    Initialisation du projet et réalisation de l'exercice 1 et 2

---
+++ TD1/script.js
@@ -0,0 +1,11 @@
+/*Exo 1 : Convertion Celsius à Fahrenheit*/
+const temperature = 20;
+const fahrenheit = (temperature * 9/5) + 32;
+document.getElementById("output1").innerText = `La température est ${fahrenheit}°F`;
+
+/*Exo 2 : Calcul de surface*/
+const longueur = 100;
+const largeur = 50;
+const aire = longueur * largeur;
+document.getElementById("output2").innerText = `La surface est ${aire} m²`;
+
